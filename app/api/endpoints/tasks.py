a549fa647fe59ee8b8bfe581beb2ccf6a914740e
alexander <sadikov.af@ssmu.ru>
2025-08-22 10:40:18 +0700

    add endpoints

---
+++ app/api/endpoints/tasks.py
@@ -0,0 +1,79 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from uuid import uuid4
+
+from app.models.tasks import TaskFromDB, TaskCreate, TaskUpdate
+from fake_db.fake_dao import get_task_dao, TasksDAO
+
+
+tasks_router = APIRouter(
+    prefix="/task",
+    tags=["Task"],
+)
+
+
+@tasks_router.post("", response_model=TaskFromDB | dict, status_code=status.HTTP_201_CREATED)
+async def create_task(task_data: TaskCreate,
+                      task_dao: TasksDAO = Depends(get_task_dao)):
+    task_id = uuid4()
+    name, description, state = task_data.model_dump().values()
+
+    task_dao.add_one((str(task_id), name, description, state.value))
+
+    return TaskFromDB(
+        id=task_id,
+        name=name,
+        description=description,
+        status=state
+    )
+
+
+@tasks_router.get("/{task_id}", response_model=TaskFromDB | dict, status_code=status.HTTP_200_OK)
+async def get_task(task_id: str,
+                   task_dao: TasksDAO = Depends(get_task_dao)):
+
+    task_from_db = task_dao.get_one("id", task_id)
+
+    if task_from_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task with id {task_id} not found")
+    else:
+        return task_from_db
+
+
+@tasks_router.delete("/{task_id}", status_code=status.HTTP_202_ACCEPTED)
+async def delete_task(task_id: str,
+                      task_dao: TasksDAO = Depends(get_task_dao)):
+    task_from_db = task_dao.delete_one("id", task_id)
+
+    if task_from_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task with id {task_id} not found")
+    else:
+        return {"detail": f"Task with id {task_id} successfully deleted"}
+
+
+@tasks_router.put("/{task_id}", status_code=status.HTTP_202_ACCEPTED)
+async def update_task(task_id: str,
+                      task_data: TaskUpdate,
+                      task_dao: TasksDAO = Depends(get_task_dao)):
+    task_from_db = task_dao.update_one("id", task_id, task_data.model_dump())
+
+    if task_from_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task with id {task_id} not found")
+    else:
+        return {"detail": f"Task with id {task_id} successfully updated"}
+
+
+@tasks_router.get("/{offset}/{limit}", response_model=list[TaskFromDB])
+async def get_reports_pack(offset: str,
+                           limit: str,
+                           task_dao: TasksDAO = Depends(get_task_dao),
+                           ):
+    tasks_list_from_db = task_dao.get_pack(int(offset),int(limit))
+
+    if tasks_list_from_db:
+        return tasks_list_from_db
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task list is empty")
---
+++ main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.api.endpoints.tasks import tasks_router
+from app.utils import create_necessary_catalogs, create_fake_db
+from app.settings.initial_settings import AppParams
+from app.settings.logging_settings import configure_logger
+
+
+
+app = FastAPI()
+
+
+app.include_router(tasks_router)
+
+
+if __name__ == "__main__":
+    create_necessary_catalogs()
+    create_fake_db()
+    logger = configure_logger(__name__)
+    logger.info(f"Starting app with {AppParams}")
+
+    uvicorn.run(AppParams.app,
+                host=AppParams.host,
+                port=AppParams.port,
+                reload=AppParams.reload,
+                workers=AppParams.workers,
+                )
