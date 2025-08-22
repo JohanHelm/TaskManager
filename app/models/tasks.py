33d21e2ca5cdf1b6b89d32279b15a7a37bc995b1
alexander <sadikov.af@ssmu.ru>
2025-08-22 10:40:56 +0700

    add db models

---
+++ app/models/tasks.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, ConfigDict
+import enum
+from typing import Optional
+from uuid import UUID
+
+class StatusEnum(enum.Enum):
+    created = "created"
+    in_progress = "in progress"
+    completed = "completed"
+
+
+class TaskBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TaskCreate(TaskBase):
+    name: str
+    description: str
+    status: StatusEnum
+
+
+class TaskFromDB(TaskCreate):
+    id: UUID
+
+
+class TaskUpdate(TaskBase):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[StatusEnum] = None
